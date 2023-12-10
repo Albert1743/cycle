8467fc9c7384f9b5afa321084488695fa7ca259e
ali <mustafaevalibek52@gmail.com>
2023-12-11 01:19:50 +0500

    work

--- script.js
+++ script.js
@@ -11,22 +11,6 @@
 
 
 
-let corrPass = 1231
-let attempts = 5
-
-do {
-    let ask = prompt('Введите пароль')
-    if (ask === corrPass) {
-        alert("Пароль верный. Доступ разрешен!")
-    } else {
-        alert(`Неверный пароль, осталось попыток: ${attempts}`)
-    } while (attempts > 0)
-    if (attempts === 0) {
-        alert('У вас закончились попытки. Доступ запрещен.')
-    }
-}
-
-
 // let corrPass = "1231"
 // let attempts = 5
 // let enterPass;
@@ -44,3 +28,112 @@ do {
 // if (attempts === 0) {
 //     alert('У вас закончились попытки. Доступ запрещен.')
 // }
+
+
+
+// let arr = ['alibek', 'hasan', 'daler', 'bexruz', 'jaxongir']
+// let na = ""
+
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i].length > na.length) {
+//         na = arr[i]
+//     }
+// }
+// console.log('the longest name is: ' + na);
+
+// for (let item of arr) {
+//     if (item.length > na.length) {
+//         na = item
+//     }
+// }
+// console.log('the longest name is: ' + na);
+
+
+
+// let evens = []
+// for (i = 0; i < 100; i++) {
+//     if (i % 2 === 0) {
+//         evens.push(i)
+//     }
+// }
+// console.log(evens);
+
+
+
+let students = [
+    {
+        name: "Alsafir",
+        age: 15,
+        gender: 'm'
+    },
+    {
+        name: "Hasan",
+        age: 15,
+        gender: 'm'
+    },
+    {
+        name: "Allayor",
+        age: 18,
+        gender: 'w'
+    },
+    {
+        name: "Radmir",
+        age: 11,
+        gender: 'w'
+    },
+    {
+        name: "Jaxongir",
+        age: 17,
+        gender: 'm'
+    },
+    {
+        name: "Bexruz",
+        age: 16,
+        gender: 'w'
+    },
+    {
+        name: "Alibek",
+        age: 17,
+        gender: 'm'
+    },
+    {
+        name: "Daler",
+        age: 19,
+        gender: 'w'
+    },
+    {
+        name: "Aziz",
+        age: 22,
+        gender: 'w'
+    }
+]
+
+let m = 0
+let w = 0
+
+for (let i = 0; i < students.length; i++) {
+    switch (students[i].gender) {
+        case 'm':
+            m++;
+            break;
+        case 'w':
+            w++;
+            break;
+        default:
+            break;
+    }
+}
+console.log('Number of male:', m);
+console.log('Number of female:', w);
+
+
+let average_age = 0
+
+for (let i = 0; i < students.length; i++) {
+    average_age = students.reduce((a, b) => {
+        return a + b.age
+    }, 0)
+}
+average_age = average_age / students.length 
+console.log('Average age:',average_age);
+
